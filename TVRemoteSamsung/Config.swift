9c0104a1744dd7bf67862c17808c7b6f74cc6644
AnnieKov <80928101+AnnieKov@users.noreply.github.com>
2025-05-11 13:42:22 +0300

    upd

--- TVRemoteSamsung/Config.swift
+++ TVRemoteSamsung/Config.swift
@@ -3,8 +3,8 @@ import UIKit
 
 enum Config {
     
-    static let terms = "https://docs.google.com/document/d/1VSEDqIPwPTVjR8MSKCYpF9qp0xB_ERt6LEDBqWTfxRg/edit?usp=sharing"
-    static let privacy = "https://docs.google.com/document/d/1CpRctMr8Sja16--Dru_axn30MSA46pRbmgFABu4RjRM/edit?usp=sharing"
+    static let terms = "https://docs.google.com/document/d/1smtYhEJuHGpNLC7eEKqj4WNhrOXKziNk2qU7CIod_s8/edit?usp=sharing"
+    static let privacy = "https://docs.google.com/document/d/1VzddCjyGXhRuVfe6bFMS8AMwSzmrR1k3cy_X3FczQdo/edit?usp=sharing"
     static let apphudKey = "app_KnhpwhQvNR3fHNjcuZM6vWLpUpJiD8"
     static let appId = "6745526464"
     static let appName = "Samsung Remote TV iOS App"
